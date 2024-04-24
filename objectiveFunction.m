e708ead3dfdf8468f65f5dc0faf1b6061bf4100b
CannedOrgi <114853288+CannedOrgi@users.noreply.github.com>
2024-04-24 20:54:05 +0530

    Update objectiveFunction.m

--- objectiveFunction.m
+++ objectiveFunction.m
@@ -1,10 +1,9 @@
 function f = objectiveFunction(weights, sharpes)
-    % Calculate the weighted sum of sharpes squared
+    % Calculate the weighted sum of sharpes
     weightedSharpes = sum(weights .* (sharpes));
     penalty = 0.015 * std(weights);  % A penalty for lack of diversification of the portfolio
     % Number of sharpes
     n = numel(sharpes);
-    % Calculate the root mean square
   
     f = -(weightedSharpes/n - penalty);
 end
